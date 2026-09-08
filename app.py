537555cd650b43d547daf8786a74c92ade8ba4a8
Yonathan Arevalo <yonathanarevalocastillo@gmail.com>
2026-09-08 09:58:32 -0500

    Agregar imagenes de productos

--- app.py
+++ app.py
@@ -4,22 +4,22 @@ app = Flask(__name__)
 
 hombres = [
     
-    {"nombre": "White Street", "precio": "$105.000", "imagen": "/static/1.png"},
-    {"nombre": "Classic Urban", "precio": "$110.000", "imagen": "/static/2.png"},
-    {"nombre": "Classic 1", "precio": "$150.000", "imagen": "/static/3.png"},
-    {"nombre": "Classic 2", "precio": "$180.000", "imagen": "/static/4.png"},
-    {"nombre": "Classic 3", "precio": "$200.000", "imagen": "/static/5.png"},
-    {"nombre": "Classic 3", "precio": "$200.000", "imagen": "/static/6.png"},
-    {"nombre": "Classic 3", "precio": "$200.000", "imagen": "/static/7.png"},
+    {"nombre": "White Street", "precio": "$105.000", "imagen": "/static/imagen1.png"},
+    {"nombre": "Classic Urban", "precio": "$110.000", "imagen": "/static/imagen2.png"},
+    {"nombre": "Classic 1", "precio": "$150.000", "imagen": "/static/imagen3.png"},
+    {"nombre": "Classic 2", "precio": "$180.000", "imagen": "/static/imagen4.png"},
+    {"nombre": "Classic 3", "precio": "$200.000", "imagen": "/static/imagen5.png"},
+    {"nombre": "Classic 3", "precio": "$200.000", "imagen": "/static/imagen6.png"},
+    {"nombre": "Classic 3", "precio": "$200.000", "imagen": "/static/imagen7.png"},
     
 ]
 
 damas = [
-    {"nombre": "Classic 4", "precio": "$200.000", "imagen": "/static/8.png"},
-    {"nombre": "Classic 4", "precio": "$200.000", "imagen": "/static/9.png"},
-    {"nombre": "Classic 5", "precio": "$200.000", "imagen": "/static/10.png"},
-    {"nombre": "Classic 6", "precio": "$200.000", "imagen": "/static/12.png"},
-    {"nombre": "Classic 6", "precio": "$200.000", "imagen": "/static/13.png"}, 
+    {"nombre": "Classic 4", "precio": "$200.000", "imagen": "/static/imagen8.png"},
+    {"nombre": "Classic 4", "precio": "$200.000", "imagen": "/static/imagen9.png"},
+    {"nombre": "Classic 5", "precio": "$200.000", "imagen": "/static/imagen10.png"},
+    {"nombre": "Classic 6", "precio": "$200.000", "imagen": "/static/imagen12.png"},
+    {"nombre": "Classic 6", "precio": "$200.000", "imagen": "/static/imagen13.png"}, 
      
 ]
 
