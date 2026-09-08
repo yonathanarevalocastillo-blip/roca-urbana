7cd6e8484187bc7fd39fba9fa2578fce83bd0fa3
Yonathan Arevalo <yonathanarevalocastillo@gmail.com>
2026-09-08 09:51:31 -0500

    Actualizar productos e imagenes

--- app.py
+++ app.py
@@ -3,18 +3,24 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 hombres = [
-    {"nombre": "Urban Black", "precio": "$99.000", "imagen": "/static/13.png"},
-    {"nombre": "White Street", "precio": "$105.000", "imagen": "/static/4.png"},
-    {"nombre": "Classic Urban", "precio": "$110.000", "imagen": "/static/5.png"},
-    {"nombre": "Classic 1", "precio": "$150.000", "imagen": "/static/6.png"},
-    {"nombre": "Classic 2", "precio": "$180.000", "imagen": "/static/7.png"},
-    {"nombre": "Classic 3", "precio": "$200.000", "imagen": "/static/8.png"},
+    
+    {"nombre": "White Street", "precio": "$105.000", "imagen": "/static/1.png"},
+    {"nombre": "Classic Urban", "precio": "$110.000", "imagen": "/static/2.png"},
+    {"nombre": "Classic 1", "precio": "$150.000", "imagen": "/static/3.png"},
+    {"nombre": "Classic 2", "precio": "$180.000", "imagen": "/static/4.png"},
+    {"nombre": "Classic 3", "precio": "$200.000", "imagen": "/static/5.png"},
+    {"nombre": "Classic 3", "precio": "$200.000", "imagen": "/static/6.png"},
+    {"nombre": "Classic 3", "precio": "$200.000", "imagen": "/static/7.png"},
+    
 ]
 
 damas = [
+    {"nombre": "Classic 4", "precio": "$200.000", "imagen": "/static/8.png"},
     {"nombre": "Classic 4", "precio": "$200.000", "imagen": "/static/9.png"},
     {"nombre": "Classic 5", "precio": "$200.000", "imagen": "/static/10.png"},
     {"nombre": "Classic 6", "precio": "$200.000", "imagen": "/static/12.png"},
+    {"nombre": "Classic 6", "precio": "$200.000", "imagen": "/static/13.png"}, 
+     
 ]
 
 @app.route("/")
