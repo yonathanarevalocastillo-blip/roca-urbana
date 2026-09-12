fb026988530fe0f788f7dc76eb316dd1190b705c
Yonathan Arevalo <yonathanarevalocastillo@gmail.com>
2026-09-11 20:12:32 -0500

    Actualizar nombres de productos

--- app.py
+++ app.py
@@ -10,16 +10,16 @@ hombres = [
     {"nombre": "AIR JORDAN 4", "precio": "$100.000", "imagen": "/static/imagen5.png"},
     {"nombre": "AIR JORDAN RETRO 3", "precio": "$120.000", "imagen": "/static/imagen6.png"},
     {"nombre": "AIR JORDAN RETRO 11", "precio": "$140.000", "imagen": "/static/imagen7.png"},
-    {"nombre": "AIR JORDAN RETRO 11", "precio": "$140.000", "imagen": "/static/imagen71.png"},
-    {"nombre": "AIR JORDAN RETRO 11", "precio": "$140.000", "imagen": "/static/imagen72.png"},
-    {"nombre": "AIR JORDAN RETRO 11", "precio": "$140.000", "imagen": "/static/imagen73.png"},
-    {"nombre": "AIR JORDAN RETRO 11", "precio": "$140.000", "imagen": "/static/imagen74.png"},
-    {"nombre": "AIR JORDAN RETRO 11", "precio": "$140.000", "imagen": "/static/imagen75.png"},
-    {"nombre": "AIR JORDAN RETRO 11", "precio": "$140.000", "imagen": "/static/imagen76.png"},
-    {"nombre": "AIR JORDAN RETRO 11", "precio": "$140.000", "imagen": "/static/imagen77.png"},
-    {"nombre": "AIR JORDAN RETRO 11", "precio": "$140.000", "imagen": "/static/imagen78.png"},
-    {"nombre": "AIR JORDAN RETRO 11", "precio": "$140.000", "imagen": "/static/imagen79.png"},
-    
+    {"nombre": "AIR JORDAN RETRO 3", "precio": "$120.000", "imagen": "/static/imagen71.png"},
+    {"nombre": "AIR JORDAN RETRO 1 LOW", "precio": "$120.000", "imagen": "/static/imagen72.png"},
+    {"nombre": "LECOQ SPORTIF", "precio": "$110.000", "imagen": "/static/imagen73.png"},
+    {"nombre": "PUMA SUEDE X BMX", "precio": "$120.000", "imagen": "/static/imagen74.png"},
+    {"nombre": "LECOQ SPORTIF", "precio": "$100.000", "imagen": "/static/imagen75.png"},
+    {"nombre": "VANS HYLANE", "precio": "$120.000", "imagen": "/static/imagen76.png"},
+    {"nombre": "OFF WHITE", "precio": "$120.000", "imagen": "/static/imagen77.png"},
+    {"nombre": "ADIDAS SUPERSTAR", "precio": "$120.000", "imagen": "/static/imagen78.png"},
+    {"nombre": "NIKE CORTEZ", "precio": "$110.000", "imagen": "/static/imagen79.png"},
+        
 ]
 
 damas = [
